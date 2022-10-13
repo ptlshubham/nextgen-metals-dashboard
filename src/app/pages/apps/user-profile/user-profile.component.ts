039cbae1bb2b9d7f1c3d11ac4e719d8b702b4591
Shubham Patel <ptlshubham@gmail.com>
2022-10-13 15:01:55 +0530

    Changes

--- src/app/pages/apps/user-profile/user-profile.component.ts
+++ src/app/pages/apps/user-profile/user-profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from '../../form/validation/validation.mustmatch';
 
 @Component({
   selector: 'app-user-profile',
@@ -33,6 +34,26 @@ export class UserProfileComponent implements OnInit {
       { label: 'Contacts' },
       { label: 'Profile', active: true }
     ];
+
+    this.validationForm = this.formBuilder.group({
+      fname: ['Shubham', [Validators.required]],
+      lname: ['Patel', [Validators.required]],
+      contact: ['8141952604', [Validators.required, Validators.min(1)]],
+      email: ['info@nextgenmetals.com', [Validators.required, Validators.email]],
+      companyname: ['Nextgen Metals', [Validators.required]],
+      desgination: ['Manager', [Validators.required]],
+      gstno: ['39294SS42MSB', [Validators.required]],
+      workphone: ['701644159', [Validators.required, Validators.min(1)]],
+      address: ['32a, Knowledge Park 1, Greater Noida, Noida Delhi, India, 201301', [Validators.required]],
+
+      currentpwd: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmpwd: ['', Validators.required]
+
+    }, {
+      validator: MustMatch('password', 'confirmpwd'),
+
+    });
   }
   get f() { return this.validationForm.controls; }
 
