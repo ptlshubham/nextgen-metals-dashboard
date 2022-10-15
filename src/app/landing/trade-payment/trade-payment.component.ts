592080e9bfb22966b43f39d24d4e9831b01d432e
Shubham Patel <ptlshubham@gmail.com>
2022-10-15 17:00:52 +0530

    File Upload

--- src/app/landing/trade-payment/trade-payment.component.ts
+++ src/app/landing/trade-payment/trade-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-trade-payment',
@@ -6,10 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trade-payment.component.scss']
 })
 export class TradePaymentComponent implements OnInit {
-
+  @ViewChild('fileInput') el!: ElementRef;
+  imageUrl: any = "assets/images/file-upload-image.jpg";
+  editFile: boolean = true;
+  removeUpload: boolean = false;
+  cardImageBase64: any;
+  materialImage: any;
   constructor() { }
 
   ngOnInit(): void {
   }
+  uploadFile(event: any) {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
 
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        const imgBase64Path = reader.result;
+        this.cardImageBase64 = imgBase64Path;
+        const formdata = new FormData();
+        formdata.append('file', file);
+        debugger
+
+        // this.sellerTradeService.uploadMaterialImage(formdata).subscribe((response) => {
+        //   this.materialImage = response;
+        //   this.editFile = false;
+        //   this.removeUpload = true;
+        // })
+      }
+      // ChangeDetectorRef since file is loading outside the zone
+      // this.cd.markForCheck();
+
+    }
+  }
+
+  // Function to remove uploaded file
+  removeUploadedFile() {
+    let newFileList = Array.from(this.el.nativeElement.files);
+    this.imageUrl = 'https://i.pinimg.com/236x/d6/27/d9/d627d9cda385317de4812a4f7bd922e9--man--iron-man.jpg';
+    this.editFile = true;
+    this.removeUpload = false;
+
+  }
 }
