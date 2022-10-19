354e8714024c3a6e559836fef0e1f5bda081ea37
Shubham Patel <ptlshubham@gmail.com>
2022-10-19 10:27:30 +0530

    Field Add in Register

--- src/app/extrapages/user-register/user-register.component.ts
+++ src/app/extrapages/user-register/user-register.component.ts
@@ -15,8 +15,8 @@ export class UserRegisterComponent implements OnInit {
   submitted = false;
   constructor(
     public formBuilder: FormBuilder,
-    public userservice:UserProfileService,
-    public router:Router
+    public userservice: UserProfileService,
+    public router: Router
   ) { }
 
   ngOnInit(): void {
@@ -33,9 +33,10 @@ export class UserRegisterComponent implements OnInit {
       gstno: ['', [Validators.required]],
       workphone: ['', [Validators.required, Validators.min(1)]],
       address: ['', [Validators.required]],
+      avg_mnth_trade: ['0', [Validators.required, Validators.min(1)]],
       selectMaterial: ['', [Validators.required]],
 
-      selectAcc:['',[Validators.required]],
+      selectAcc: ['', [Validators.required]],
       acHolder: ['', [Validators.required]],
       bankName: ['', [Validators.required]],
       bankACNo: ['', [Validators.required, Validators.min(1)]],
@@ -48,13 +49,13 @@ export class UserRegisterComponent implements OnInit {
     this.submitted = true;
     this.validationForm.value;
     // stop here if form is invalid
-      this.userservice.registerUser(this.validationForm.value).subscribe((res:any)=>{
-        if(res == 'sucess'){
-          Swal.fire('Successfully!', 'Regsitration completed and wait for KYC updation.Password will mail to you shortly', 'success');
-          this.router.navigate(['pages/home']);
-        }else{
-          Swal.fire('oops!', 'Try after sometime', 'success');
-        }
-      })
+    this.userservice.registerUser(this.validationForm.value).subscribe((res: any) => {
+      if (res == 'sucess') {
+        Swal.fire('Successfully!', 'Regsitration completed and wait for KYC updation.Password will mail to you shortly', 'success');
+        this.router.navigate(['pages/home']);
+      } else {
+        Swal.fire('oops!', 'Try after sometime', 'success');
+      }
+    })
   }
 }
