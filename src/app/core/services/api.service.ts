bb07ce3431e1028c54ff3d5e76e1d9322dbfdb0b
Shubham Patel <ptlshubham@gmail.com>
2022-10-20 11:38:25 +0530

    Cancel Check Image Upload

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { HttpClient, HttpClientModule, HttpHeaders, HTTP_INTERCEPTORS } from '@angular/common/http';
-import {  TemplateRef } from '@angular/core';
+import { TemplateRef } from '@angular/core';
 declare var $: any;
 @Injectable({
   providedIn: 'root'
@@ -17,7 +17,7 @@ export class ApiService {
       'Content-Type': 'application/json'
     })
   }
-  
+
   public static userLoginURL: string = ApiService.HOST_URL + '/authenticate/userLogin';
   public static adminLoginURL: string = ApiService.HOST_URL + '/authenticate/adminLogin';
   public static RegisterNewCustomerURL: string = ApiService.HOST_URL + '/admin/RegisterNewUser';
@@ -27,19 +27,20 @@ export class ApiService {
   public static getKycPendingListURL: string = ApiService.HOST_URL + '/admin/getAllKYCPendingUser';
   public static updateKYCURL: string = ApiService.HOST_URL + '/admin/updateKYCUser';
   public static uploadMaterialImageURL: string = ApiService.HOST_URL + '/admin/UploadMaterialImage';
-  
- 
- 
- 
- 
- 
+  public static uploadCancelCheckImageURL: string = ApiService.HOST_URL + '/admin/UploadCancelCheckImage';
+
+
+
+
+
+
 
 
   //Cashfree APIS
   public static createCashfreeOrderURL: string = ApiService.HOST_URL + '/cashfree/createCashfreeOrder';
 
 
- 
+
 
   show(textOrTpl: string | TemplateRef<any>, options: any = {}) {
     debugger
