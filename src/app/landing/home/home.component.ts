c145070124d6dbe2735dffd5a3f8552eed7aef71
Shubham Patel <ptlshubham@gmail.com>
2022-11-21 17:46:29 +0530

    Buyer Home

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -12,64 +12,81 @@ export class HomeComponent implements OnInit {
   openTSummary: boolean = false;
   openPaymentSumm: boolean = false;
   role: any;
-  allTradingListBuyer:any=[];
-  newTradingListBuyer:any=[];
-  weekTradeCountBuyer:number=0;
-  allTradingListSeller:any=[];
-  newTradingListSeller:any=[];
-  weekTradeCountSeller:number=0;
+  allTradingListBuyer: any = [];
+  newTradingListBuyer: any = [];
+  weekTradeCountBuyer: number = 0;
+  allTradingListSeller: any = [];
+  newTradingListSeller: any = [];
+  weekTradeCountSeller: number = 0;
+  tradeSummaryCount: any = [];
   constructor(
     private router: Router,
-    private tradingService:TradeService
+    private tradingService: TradeService
   ) { }
 
   ngOnInit(): void {
     this.role = localStorage.getItem('Role');
-    if(this.role =='buyer'){
-      this.tradingService.getAllTradingDatabyIdForBuyer(localStorage.getItem('UserId')).subscribe((res:any)=>{
+    if (this.role == 'buyer') {
+      this.tradingService.getAllTradingDatabyIdForBuyer(localStorage.getItem('UserId')).subscribe((res: any) => {
         debugger
-         if(res.length ==0){
-          this.allTradingListBuyer.length=0;
-         }else{
+
+        if (res.length == 0) {
+          this.allTradingListBuyer.length = 0;
+        } else {
           this.allTradingListBuyer = res;
-          this.allTradingListBuyer.forEach((element:any) => {
-            if(element.tradeStatus == 'IDEAL'){
+          this.tradeSummaryCount = [];
+          this.allTradingListBuyer.forEach((element: any) => {
+            if (element.tradeStatus == 'ACCEPTED') {
+              debugger
+              this.tradeSummaryCount.push(element);
+            }
+          });
+          this.allTradingListBuyer.forEach((element: any) => {
+            if (element.tradeStatus == 'IDEAL') {
               this.newTradingListBuyer.push(element);
             }
             let olddate = this.getLastWeeksDate().toISOString();
-            if(element.createdDate >=olddate){
+            if (element.createdDate >= olddate) {
               this.weekTradeCountBuyer++;
             }
           });
-         }
-        
+        }
+
       })
-    }else{
-      this.tradingService.getAllTradingDatabyIdForSeller(localStorage.getItem('UserId')).subscribe((res:any)=>{
+    } else {
+      this.tradingService.getAllTradingDatabyIdForSeller(localStorage.getItem('UserId')).subscribe((res: any) => {
         debugger
-        if(res.length ==0){
-          this.allTradingListSeller.length=0;
-        }else{
+     
+        if (res.length == 0) {
+          this.allTradingListSeller.length = 0;
+        } else {
           this.allTradingListSeller = res;
-          this.allTradingListSeller.forEach((element:any) => {
-            if(element.tradeStatus == 'IDEAL'){
+          this.tradeSummaryCount = [];
+          this.allTradingListBuyer.forEach((element: any) => {
+            if (element.tradeStatus == 'ACCEPTED') {
+              debugger
+              this.tradeSummaryCount.push(element);
+            }
+          });
+          this.allTradingListSeller.forEach((element: any) => {
+            if (element.tradeStatus == 'IDEAL') {
               this.newTradingListSeller.push(element);
             }
             let olddate = this.getLastWeeksDate().toISOString();
-            if(element.createdDate >=olddate){
+            if (element.createdDate >= olddate) {
               this.weekTradeCountSeller++;
             }
           });
         }
-      
+
       })
     }
-  
-    
+ 
+
   }
   getLastWeeksDate() {
     const now = new Date();
-  
+
     return new Date(now.getFullYear(), now.getMonth(), now.getDate() - 7);
   }
 
@@ -77,10 +94,10 @@ export class HomeComponent implements OnInit {
     this.openNewRequest = true;
     this.openTSummary = false;
     this.openPaymentSumm = false;
-    if(localStorage.getItem('Role')=='buyer'){
+    if (localStorage.getItem('Role') == 'buyer') {
       this.router.navigate(['/landing/trade-active']);
     }
-    
+
   }
   openTradeSummary() {
     this.openTSummary = true;
